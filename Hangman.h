3699a7b4eedf4d501b91515d183583fcb00d5df5
mdenardis <mdenardis@users.noreply.github.com>
2015-04-21 23:16:43 -0600

    Added Hangman.h header file for game

---
+++ Hangman.h
@@ -0,0 +1,30 @@
+#ifndef HANGMAN_H
+#define HANGMAN_H
+#pragma once              //Necessary preprocessor directives for following components
+#include <string>
+
+class Hangman {  //Declares a class called Hangman with private data members and public member functions
+
+    private:
+        std::string name;
+        std::string wordsFile;
+        int level;
+        int totalTries;
+
+	public:
+        Hangman();                //Default constructor
+        virtual ~Hangman();
+        std::string getName() const {return name;}           //Accessor methods or get functions
+        std::string getWordsFile() const {return wordsFile;}
+        int getLevel() const {return level;}
+        int getTotalTries() const {return totalTries;}
+        void loadDirections() const;                     //Function prototypes for game components
+        void enterName();
+        void chooseLevel();
+        void loadWords();
+        void scrambler();
+        void results() const;
+};
+
+
+#endif // HANGMAN_H
