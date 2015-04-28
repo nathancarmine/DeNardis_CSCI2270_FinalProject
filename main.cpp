8d36c9428d6b75d92132d7ba730756f40761ece9
Michelle De Nardis <mdenardis@users.noreply.github.com>
2015-04-27 21:36:42 -0600

    Final main.cpp, removed some function calls

--- main.cpp
+++ main.cpp
@@ -14,8 +14,9 @@
 using namespace std;
 
 int main() {
-	Hangman game;           //Instantiates object game of class Game.
-	srand(time(NULL));      //Scrambles the words differently each time the game is played.
+
+	Hangman game;       //Instantiates object game of class Hangman.
+	srand(time(NULL));      //Generates random integer each time game is played to choose random word from text file
 
 	cout << "Welcome to the Hangman Game!" << endl;            //Prints message to console.
 	game.loadDirections();                                     //Calls loadDirections function on object game, prints directions to console.
@@ -23,11 +24,6 @@ int main() {
 	game.enterName();                                          //Calls enterName function, allows user to enter their name.
 	cout << endl;
 
-    game.initializeGame();
-
-	cout << "Congratulations " << game.getName() << ", you've finished the game!" << endl;            //Again prints the user's name with message.
-	//cout << "You used a total of " << game.getTotalTries() << " tries. You're score is " << game.getTotalScore() << "." << endl;  //Calls getTotalTries which prints the total number of tries for the whole game
-	                                                                                                                              //and getTotalScore which prints final score to the console.
-	//game.results();  //Calls results function which prints final statement based on score.
+    	game.initializeGame(); //calls initializeGame function which calls hangmanGame function and takes over running the game
 
 }
